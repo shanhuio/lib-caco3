91832c4a0b121b8dfe9de0d20af30e0c9ef2c3eb
Lonnie Liu <liulonnie@gmail.com>
2021-08-06 17:01:24 -0700

    use different esbuild package for different arch

--- docker_nodejs.go
+++ docker_nodejs.go
@@ -16,24 +16,55 @@
 package elsa
 
 import (
+	"bytes"
+	"runtime"
+	"text/template"
+
+	"shanhu.io/misc/errcode"
 	"shanhu.io/misc/tarutil"
 	"shanhu.io/virgo/dock"
 )
 
-const nodejsDockerfile = `
+const nodejsDockerfileContent = `
 FROM cr.shanhu.io/base/alpine
 MAINTAINER Shanhu Tech Inc.
 
 RUN apk add --update \
 	nodejs npm make git openssh zip
 RUN npm install -g npm
-RUN npm install -g typescript esbuild-linux-64 less
+RUN npm install -g typescript less {{.Esbuild}}
 
 RUN mkdir /usr/local/idle
 COPY idle.js /usr/local/idle/idle.js
 
 CMD ["/usr/bin/node", "/usr/local/idle/idle.js"]
 `
+
+var nodejsDockerfileTmpl = template.Must(
+	template.New("_").Parse(nodejsDockerfileContent),
+)
+
+func makeNodeJSDockerFile() (string, error) {
+	buf := new(bytes.Buffer)
+
+	var dat struct {
+		Esbuild string
+	}
+
+	switch runtime.GOARCH {
+	case "amd64":
+		dat.Esbuild = "esbuild-linux-64"
+	case "arm64":
+		dat.Esbuild = "esbuild-linux-arm64"
+	default:
+		dat.Esbuild = "esbuild" // might not work..
+	}
+
+	if err := nodejsDockerfileTmpl.Execute(buf, &dat); err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
 
 const idleJS = `
 function f() { setTimeout(f, 60*60*1000) }
@@ -48,7 +79,11 @@ f()`
 var dockerNodejs = &baseDocker{
 	name: "base/nodejs",
 	build: func(env *env, name string) error {
-		ts := dock.NewTarStream(nodejsDockerfile)
+		dockerFile, err := makeNodeJSDockerFile()
+		if err != nil {
+			return errcode.Annotate(err, "make DockerFile")
+		}
+		ts := dock.NewTarStream(dockerFile)
 		ts.AddString("idle.js", tarutil.ModeMeta(0600), idleJS)
 		return buildDockerImage(env, name, nil, ts)
 	},
