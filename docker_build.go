115da509db471fbb2c36139c6be6754c0f9cf721
Lonnie Liu <liulonnie@gmail.com>
2022-04-05 22:46:01 -0700

    remove docker file parsing

--- docker_build.go
+++ docker_build.go
@@ -16,7 +16,6 @@
 package caco3
 
 import (
-	"bufio"
 	"os"
 	"path"
 	"sort"
@@ -29,65 +28,11 @@ import (
 	"shanhu.io/virgo/dock"
 )
 
-type dockerfile struct {
-	from      []string
-	fromRules []string
-	inputs    []string
-}
-
-func parseLineWithPrefix(line, pre string) (string, bool) {
-	if !strings.HasPrefix(line, pre) {
-		return "", false
-	}
-	return strings.TrimSpace(strings.TrimPrefix(line, pre)), true
-}
-
-func readDockerFile(file, p string) (*dockerfile, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, errcode.Annotate(err, "open Dockerfile")
-	}
-	defer f.Close()
-
-	df := new(dockerfile)
-	s := bufio.NewScanner(f)
-	for s.Scan() {
-		line := strings.TrimSpace(s.Text())
-		if line == "" {
-			continue
-		}
-
-		if v, ok := parseLineWithPrefix(line, "#in "); ok {
-			df.inputs = append(df.inputs, makePath(p, v))
-		} else if v, ok := parseLineWithPrefix(line, "#from "); ok {
-			df.fromRules = append(df.fromRules, makePath(p, v))
-		} else if v, ok := parseLineWithPrefix(line, "FROM "); ok {
-			comment := strings.Index(v, "#")
-			if comment >= 0 {
-				v = strings.TrimSpace(v[:comment])
-			}
-			fields := strings.Fields(v)
-			if len(fields) > 0 {
-				df.from = append(df.from, fields[0])
-			}
-		} else if strings.HasPrefix(line, "#") {
-			continue
-		} else {
-			break
-		}
-	}
-	if err := s.Err(); err != nil {
-		return nil, errcode.Annotate(err, "scan Dockerfile")
-	}
-	return df, nil
-}
-
 type dockerBuild struct {
 	name           string
 	rule           *DockerBuild
 	fromRules      []string
 	dockerfilePath string
-	dockerfile     *dockerfile
 	inputs         []string
 	repoTag        string
 	args           map[string]string
@@ -103,27 +48,13 @@ func newDockerBuild(env *env, p string, r *DockerBuild) (
 	if r.Dockerfile == "" {
 		f = path.Join(name, "Dockerfile")
 	} else {
-		f = makeRelPath(p, r.Dockerfile)
-	}
-
-	df, err := readDockerFile(env.src(f), p)
-	if err != nil {
-		return nil, errcode.Annotate(err, "read Dockerfile")
-	}
-	if len(df.from)+len(df.fromRules) == 0 && len(r.From) == 0 {
-		return nil, errcode.Annotate(err, "FROM statement missing")
+		f = makePath(p, r.Dockerfile)
 	}
 
 	var fromRules []string
 	if len(r.From) > 0 {
-		fromRules = append(fromRules, r.From...)
-	} else {
-		for _, from := range df.from {
-			fromRule, err := env.imageRepoRule(from)
-			if err != nil {
-				return nil, errcode.Annotatef(err, "rule for %q", from)
-			}
-			fromRules = append(fromRules, fromRule)
+		for _, from := range r.From {
+			fromRules = append(fromRules, makePath(p, from))
 		}
 	}
 
@@ -138,16 +69,12 @@ func newDockerBuild(env *env, p string, r *DockerBuild) (
 	for _, input := range r.Input {
 		inputMap[makePath(p, input)] = true
 	}
-	for _, input := range df.inputs {
-		inputMap[input] = true
-	}
 
 	return &dockerBuild{
 		name:           name,
 		rule:           r,
 		dockerfilePath: f,
 		fromRules:      fromRules,
-		dockerfile:     df,
 		inputs:         strutil.SortedList(inputMap),
 		repoTag:        repoTag,
 		args:           args,
